b0e2c46cc5580b23e01c7349c5b16927566f9fc0
deniz <yilmaz.deniz.itu@gmail.com>
2022-02-13 15:42:19 +0300

    added user management

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core")
-const { v1: uuid } = require('uuid') 
+const config = require('./utils/config')
+const { ApolloServer, UserInputError, AuthenticationError, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
+const jwt = require('jsonwebtoken')
+
 const mongoose = require('mongoose')
 const Person = require('./models/person')
-const config = require('./utils/config')
+const User = require('./models/user')
 
-// const MONGODB_URI = 'mongodb+srv://fullstack:halfstack@cluster0-ostce.mongodb.net/graphql?retryWrites=true'
+const JWT_SECRET = 'NEED_HERE_A_SECRET_KEY'
 
-mongoose.connect(config.MONGODB_URI)
+const MONGODB_URI = config.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
+    console.log('error connecting to MongoDB:', error.message)
   })
 
 const typeDefs = gql`
-  type User {
-    username: String!
-    friends: [Person!]!
-    id: ID!
-  }
-
-  type Token {
-    value: String!
-  }
-
-  type Address {
-    street: String!
-    city: String!
-  }
-
   type Person {
     name: String!
     phone: String
@@ -38,56 +29,77 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type Address {
+    street: String!
+    city: String! 
+  }
+  
   enum YesNo {
     YES
     NO
   }
 
+  type User {
+    username: String!
+    friends: [Person!]!
+    id: ID!
+  }
+  
+  type Token {
+    value: String!
+  }
+
   type Query {
     personCount: Int!
-    allPersons(phone: String): [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
     me: User
   }
 
   type Mutation {
-      addPerson(
-          name: String!
-          phone: String
-          street: String!
-          city: String!
-      ): Person
-      editNumber(
-        name: String!
-        phone: String!
-      ): Person
-  }
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
+    createUser(
+      username: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+    addAsFriend(
+      name: String!
+    ): User
+  }  
 `
 
 const resolvers = {
   Query: {
     personCount: () => Person.collection.countDocuments(),
     allPersons: (root, args) => {
-      // filters missing
-      if(!args.phone){
+      console.log(root)
+      if (!args.phone) {
         return Person.find({})
       }
-      /**
-       * Mongo DB
-       * $exists
-       * { field: { $exists: <boolean> } }
-       * When <boolean> is true, $exists matches the documents that contain the field, including
-       * documents where the field value is null. If <boolean> is false, the query returns only the
-       * documents that do not contain the field.
-       * MongoDB $exists does not correspond to SQL operator exists. For SQL exists, refer to the
-       * $in operator.
-      */
-      return Person.find({ phone: { $exists: args.phone === 'YES' } })
+  
+      return Person.find({ phone: { $exists: args.phone === 'YES'  }})
     },
-    findPerson: (root, args) => Person.findOne({ name: args.name })
+    findPerson: (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => {
+      console.log(root)
+      return context.currentUser
+    }
   },
   Person: {
-    address: root => {
+    address: (root) => {
+      console.log(root)
       return {
         street: root.street,
         city: root.city
@@ -95,15 +107,24 @@ const resolvers = {
     }
   },
   Mutation: {
-    addPerson: async (root, args) => {
+    addPerson: async (root, args, context) => {
       const person = new Person({ ...args })
+      const currentUser = context.currentUser
+  
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+  
       try {
         await person.save()
+        currentUser.friends = currentUser.friends.concat(person)
+        await currentUser.save()
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
         })
       }
+  
       return person
     },
     editNumber: async (root, args) => {
@@ -117,19 +138,65 @@ const resolvers = {
           invalidArgs: args,
         })
       }
-      return person
-    }
+    },
+    createUser: (root, args) => {
+      const user = new User({ username: args.username })
+  
+      return user.save()
+        .catch(error => {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+  
+      if ( !user || args.password !== 'secret' ) {
+        throw new UserInputError("wrong credentials")
+      }
+  
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+  
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    },
+    addAsFriend: async (root, args, { currentUser }) => {
+      const nonFriendAlready = (person) => 
+        !currentUser.friends.map(f => f._id).includes(person._id)
+  
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+  
+      const person = await Person.findOne({ name: args.name })
+      if ( nonFriendAlready(person) ) {
+        currentUser.friends = currentUser.friends.concat(person)
+      }
+  
+      await currentUser.save()
+  
+      return currentUser
+    },
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground({
-      // options
-    })
-  ]
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+      const currentUser = await User
+        .findById(decodedToken.id).populate('friends')
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
 const schema = new mongoose.Schema({
     username: {
       type: String,
